97d72392cbfe050c980d843d4072edfdd878182a
Livio Bieri <livio@livio.li>
2017-04-03 23:22:41 +0200

    Fixes crawl generator function, proper yielding

--- moritz.py
+++ moritz.py
@@ -32,7 +32,7 @@ def extract_product_information(node):
     link = info_node.xpath('./h3[@class="in-title"]/a/@href')[0]
     published = node.xpath('./em[@class="fl in-date"]/text()')[0]
     price = node.xpath('./span[@class="fl in-price"]/strong/text()')[0]
-    identifier = node.xpath('./@id')
+    identifier = node.xpath('../@id')[0]
 
     return {
         identifier: {
@@ -51,19 +51,30 @@ def extract_products(tree):
 
 
 def crawl(search):
+    while True:
+        page = requests.get('http://www.tutti.ch/ganze-schweiz?q={}'.format(search))
+        tree = html.fromstring(page.content)
+        yield [extract_product_information(product) for product in extract_products(tree)]
 
-    page = requests.get('http://www.tutti.ch/ganze-schweiz?q={}'.format(search))
-    tree = html.fromstring(page.content)
-    yield [extract_product_information(result) for result in extract_products(tree)]
+
+def notify_slack(offers):
+    print(offers)  # TODO
 
 
 def crawl_forever(search, interval_every):
 
-    notified_ids = []
+    notified_ids = set()  # TODO persist
 
-    while True:
-        offers = crawl(search)
-        notified_ids = notified_ids + offers.keys()
+    for offers in crawl(search):
+        # figure out which ids have not been notified about yet
+        new_offer_ids = set([offer.keys()[0] for offer in offers])
+        unnotified_ids = new_offer_ids.difference(notified_ids)
+        notified_ids = notified_ids.union(unnotified_ids)
+
+        # only notify offers that have not been notified about
+        notify_slack([offer for offer in offers if offer.keys()[0] in unnotified_ids])
+
+        # wait for next interval
         sleep(interval_every)
 
 
@@ -84,3 +95,7 @@ def main():
 
     args = parser.parse_args()
     crawl_forever(search=args.search, interval_every=args.interval_every)
+
+
+if __name__ == "__main__":
+    main()
